c7a1cd85b4aa211026cf376adc34310d5ce9401c
Bkonoiko <167694548+Bkonoiko@users.noreply.github.com>
2024-04-21 13:56:56 -0400

    Delete bash-script directory

--- bash-script/awk-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Hello this is awk script"
-
-year=$(date | awk '{print $6}')
-month=$(date | awk '{print $2}')
-day=$(date | awk '{print $3}')
-
-echo "Today is the $day of $month, $year"
--- bash-script/test-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Hello"
-
-user=$(whoami)
-
-echo
-echo "Welcome $user"
